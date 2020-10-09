8f38029dab3ed94a0c85c0243d7ec7a3a335b06c
Jason Winters <740675@aacps.org>
2020-10-09 15:07:41 +0000

    I made variables for my numbers.. I only needed 2 one for #100 and #300

--- index.java
+++ index.java
@@ -1,3 +1,10 @@
+
+
+int col1 = 100;
+
+int col2 = 300;
+
+
 void setup() {
 	size(500, 500);
 }
@@ -15,11 +22,13 @@ void draw() {
 // For example, “variable1 = variable1 + 2;”. 
 // Try different expressions and see what happens!
 
+
   background(255);
   stroke(0);
   fill(175);
 
-
-  
- 
+ellipse(col1,col1,col1,col1);
+ellipse(col2,col1,col1,col1);
+ellipse(col1,col2,col1,col1);
+ellipse(col2,col2,col1,col1);
 }
